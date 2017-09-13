3054f36ed889fc5b139ed1dc530b697e1ce2da08
Crossley Rozario <crozario08@gmail.com>
2017-09-12 21:42:00 -0400

    implemented winningcombos

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -12,50 +12,80 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var winnerLabel: UILabel!
 
-    @IBAction func buttonSelect(_ sender: UIButton) {
+    @IBAction func buttonSelect(but sender: UIButton) {
+        playGame(buttonSelect: sender)
         
-        let buttonSelect = sender as UIButton
-        playGame(buttonSelect: buttonSelect)
+        
+        
+        
     }
     
-    var ActivePlayer = "X"
+    var activePlayer = "X"
+    var winnerFound = false
     var playerX = [Int]()
     var playerO = [Int]()
     
-    var winCombinations = [[0,1,2]]
+
     
+    var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
+    
+    func searchWinner() {
+        for arr in winningCombinations {
+            var countX = 0
+            var countO = 0
+            for i in arr {
+                if !playerX.isEmpty && playerX.contains(i) {
+                    countX+=1
+                }
+                if !playerO.isEmpty && playerO.contains(i) {
+                    countO+=1
+                }
+                if countX == 3 {
+                    winnerLabel.text?.append("X")
+//                    let title = "X won the game"
+//                    let message = "Ok"
+//                    let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//                    let okAction = UIAlertAction(title: title, style: .default, handler: nil)
+//                    ac.addAction(okAction)
+                    
+                }
+                if countO == 3 {
+                    winnerLabel.text?.append("O")
+                }
+            }
+        }
+        
+    }
+    
+    func resetBoard() {
+        
+        
+    }
     
     
     func playGame(buttonSelect: UIButton) {
-        if ActivePlayer == "X"{
+        if activePlayer == "X"{
             buttonSelect.setTitle("X", for: UIControlState.normal)
             buttonSelect.setTitleColor(UIColor.cyan, for: .normal)
             playerX.append(buttonSelect.tag)
-            ActivePlayer = "O"
+            searchWinner()
+            activePlayer = "O"
             
             
         } else {
             buttonSelect.setTitle("O", for: UIControlState.normal)
             buttonSelect.setTitleColor(UIColor.yellow, for: .normal)
             playerO.append(buttonSelect.tag)
-            ActivePlayer = "X"
+            searchWinner()
+            activePlayer = "X"
         }
         buttonSelect.isEnabled = false
     }
-    
-    func findWinner() {
-        
-        
-        
-    
-        
-    }
-
-
     
     
 }
