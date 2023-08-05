b1fed9095b3eff7ce91bb0eb752d2d2857ba07e5
Crossley Rozario <crozariodev@gmail.com>
2023-08-05 17:10:15 -0400

    fixed board design

--- TicTacToe/Views/GameView.swift
+++ TicTacToe/Views/GameView.swift
@@ -8,71 +8,86 @@
 
 import SwiftUI
 
-
 struct GameView: View {
+    @StateObject private var ticTacToeGame = TicTacToeGame()
+    
     var body: some View {
-        BoardView()
+        VStack {
+            Spacer()
+            
+            ScoreView(ticTacToeGame: ticTacToeGame)
+            
+            Spacer()
+            
+            BoardView(ticTacToeGame: ticTacToeGame)
+            
+            Spacer()
+            
+            ButtonView(ticTacToeGame: ticTacToeGame)
+            
+            Spacer()
+        }
+        .background(Color.primaryColor)
     }
 }
 
 struct BoardView: View {
-    @StateObject private var ticTacToeGame = TicTacToeGame()
+    @StateObject var ticTacToeGame: TicTacToeGame
+    let columns = [GridItem(.flexible(),spacing: 10), GridItem(.flexible(),spacing: 10), GridItem(.flexible(),spacing: 10)]
     
     var body: some View {
-        
-        VStack {
-            Spacer()
-            HStack(spacing: 50) {
-                VStack {
+        LazyVGrid(columns: columns, spacing: 10) {
+            ForEach(ticTacToeGame.game.board, id: \.self) { item in
+                SquareView(square: item)
+                    .onTapGesture {
+                        ticTacToeGame.squareClicked(square: item)
+                    }
+            }
+        }
+        .background(.black)
+        .padding()
+        .alert("Game Over", isPresented: $ticTacToeGame.game.gameEnded) {
+            Button("OK", role: .cancel) {
+                ticTacToeGame.resetBoard()
+            }
+        } message: {
+            Text(ticTacToeGame.game.gameState == .win ? "Winner is \(ticTacToeGame.game.winner == .playerOne ? "Player One" : "Player Two")" : "It's a draw")
+        }
+    }
+}
+
+struct ScoreView: View {
+    @StateObject var ticTacToeGame: TicTacToeGame
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.secondaryColor, lineWidth: 2)
+            
+            HStack(spacing: 20) {
+                VStack(spacing: 10) {
                     Text("Player One")
                     Text("\(ticTacToeGame.getPlayerOneScore())")
                 }
-                VStack {
+                .foregroundColor(.secondaryColor)
+                .bold()
+                .font(.title2)
+                
+                
+                Divider()
+                    .background(Color.secondaryColor)
+                
+                VStack(spacing: 10) {
                     Text("Player Two")
                     Text("\(ticTacToeGame.getPlayerTwoScore())")
                 }
+                .foregroundColor(.secondaryColor)
+                .bold()
+                .font(.title2)
             }
-            
-            Spacer()
-            
-            LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-                ForEach(ticTacToeGame.game.board, id: \.self) { item in
-                    SquareView(square: item)
-                        .onTapGesture {
-                            ticTacToeGame.squareClicked(square: item)
-                        }
-                }
-            }
-            .alert("Game Over", isPresented: $ticTacToeGame.game.gameEnded) {
-                Button("OK", role: .cancel) {
-                    ticTacToeGame.resetBoard()
-                }
-            } message: {
-                Text(ticTacToeGame.game.gameState == .win ? "Winner is \(ticTacToeGame.game.winner == .playerOne ? "User One" : "User Two")" : "It's a draw")
-            }
-            
-            Spacer()
-            
-            VStack(spacing: 30) {
-                Button {
-                    ticTacToeGame.resetBoard()
-                } label: {
-                    Text("Restart Game")
-                }
-                
-                Button {
-                    ticTacToeGame.resetScore()
-                } label: {
-                    Text("Reset Score")
-                }
-            }
-            
-            Spacer()
         }
+        .frame(width: 300, height: 120)
     }
-    
-    
-    
 }
 
 struct SquareView: View {
@@ -82,15 +97,61 @@ struct SquareView: View {
         ZStack {
             Rectangle()
                 .fill(.clear)
-                .border(.black, width: 2.0)
-                .frame(width: 100,height: 100)
             Text(square.symbol.rawValue)
                 .font(.system(size: 35, weight: .bold))
         }
-        .background(.white)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .aspectRatio(1, contentMode: .fit)
+        .background(Color.primaryColor)
     }
 }
 
+struct ButtonView: View {
+    @StateObject var ticTacToeGame: TicTacToeGame
+    
+    var body: some View {
+        VStack(spacing: 15) {
+            Button {
+                ticTacToeGame.resetBoard()
+            } label: {
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(.secondaryColor)
+                    Text("Restart Game")
+                }
+                .frame(width: 150, height: 50)
+                .foregroundColor(.primaryColor)
+                .bold()
+            }
+            
+            Button {
+                ticTacToeGame.resetScore()
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .foregroundColor(.secondaryColor)
+                    Text("Reset Score")
+                }
+                .frame(width: 150, height: 50)
+                .foregroundColor(.primaryColor)
+                .bold()
+            }
+        }
+    }
+}
+
+
+extension Color {
+    static var primaryColor: Color {
+//        Color(red: 246/255, green: 244/255, blue: 235/255)
+        Color(red: 241/255, green: 246/255, blue: 249/255)
+    }
+    
+    static var secondaryColor: Color {
+        Color(red: 116/255, green: 155/255, blue: 194/255)
+    }
+}
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
